9cd20cd02e4e204087992fc963fe54204cc41ff0
JaneKKTTme <tyryry221@gmail.com>
2020-06-02 02:00:29 +0300

    Add classes ReversePolishNotation and StackMachine

---
+++ src/main/java/stack_machine/ReversePolishNotation.java
@@ -0,0 +1,191 @@
+package stack_machine;
+
+import lexer.Lexem;
+import token.Token;
+
+import java.util.*;
+
+public class ReversePolishNotation {
+    private final List<Token> tokens;
+    private Integer tokenIndex;
+    private List<Token> result;
+    private Token current_token;
+    private static Map<Lexem, Integer> lexemPriority = new HashMap<>();
+
+    public ReversePolishNotation(List<Token> tokens) {
+        this.tokens = tokens;
+        this.tokenIndex = -1;
+        this.result = new ArrayList<Token>();
+        this.current_token = null;
+    }
+
+    private void iterate() {
+        tokenIndex += 1;
+        if (tokenIndex < tokens.size()) {
+            current_token = tokens.get(tokenIndex);
+        }
+    }
+
+    private static void setLexemPriority() {
+        lexemPriority.put(Lexem.ASSIGN_OP, 0);
+        lexemPriority.put(Lexem.LEFT_B, 0);
+        lexemPriority.put(Lexem.RIGHT_B, 0);
+        lexemPriority.put(Lexem.OUTPUT_OP, 0);
+        lexemPriority.put(Lexem.MORE_LOGICAL_OP, 1);
+        lexemPriority.put(Lexem.LESS_LOGICAL_OP, 1);
+        lexemPriority.put(Lexem.MORE_OR_EQUAL_LOGICAL_OP, 1);
+        lexemPriority.put(Lexem.LESS_OR_EQUAL_LOGICAL_OP, 1);
+        lexemPriority.put(Lexem.EQUAL_LOGICAL_OP, 2);
+        lexemPriority.put(Lexem.ADDITION_OP, 1);
+        lexemPriority.put(Lexem.SUBTRACTION_OP, 1);
+        lexemPriority.put(Lexem.MULTIPLICATION_OP, 2);
+        lexemPriority.put(Lexem.DIVISION_OP, 2);
+    }
+
+    public List<Token> translate() {
+        setLexemPriority();
+        iterate();
+
+        return doTranslationToRPN();
+    }
+
+    public List<Token> doTranslationToRPN() {
+        while (tokenIndex < tokens.size()) {
+            if (current_token.getLexem() == Lexem.IF_KW) {
+                result.addAll(doTranslationForIf());
+            }
+            else if (current_token.getLexem() == Lexem.WHILE_KW) {
+                result.addAll(doTranslationForWhile());
+            }
+            else {
+                result.addAll(doTranslationForExpressionOp(Lexem.SEMICOLON));
+            }
+        }
+
+        return result;
+    }
+
+    public List<Token> doTranslationForIf() {
+        List<Token> if_result = new ArrayList<>();
+        if_result.addAll(doTranslationForLogicalOp());
+        if_result.addAll(doTranslationForBody());
+
+        int false_pos = if_result.size() + result.size() + 1;
+        for (int i = 0; i < if_result.size(); i++) {
+            if (if_result.get(i).getLexem() == Lexem.GO_FALSE) {
+                if_result.set(i, new Token(Lexem.GO_FALSE, String.valueOf(false_pos)));
+            }
+        }
+        if_result.add(new Token(Lexem.GO_TRUE, "-1"));
+
+        if (tokens.get(tokenIndex).getLexem() == Lexem.ELSE_KW) {
+            if_result.addAll(doTranslationForBody());
+            if_result.add(new Token(Lexem.IF_KW, "END"));
+        }
+        int true_pos = if_result.size() + result.size();
+        for (int i = 0; i < if_result.size(); i++) {
+            if (if_result.get(i).getLexem() == Lexem.GO_TRUE) {
+                if_result.set(i, new Token(Lexem.GO_TRUE, String.valueOf(true_pos)));
+            }
+        }
+
+
+
+        return if_result;
+    }
+
+    public List<Token> doTranslationForWhile() {
+        List<Token> while_result = new ArrayList<>();
+        while_result.addAll(doTranslationForLogicalOp());
+        while_result.addAll(doTranslationForBody());
+
+        int false_pos = while_result.size() + result.size() + 1;
+        for (int i = 0; i < while_result.size(); i++) {
+            if (while_result.get(i).getLexem() == Lexem.GO_FALSE) {
+                while_result.set(i, new Token(Lexem.GO_FALSE, String.valueOf(false_pos)));
+            }
+        }
+        while_result.add(new Token(Lexem.GO_TRUE, "-1"));
+
+        int true_pos = result.size();
+        for (int i = 0; i < while_result.size(); i++) {
+            if (while_result.get(i).getLexem() == Lexem.GO_TRUE) {
+                while_result.set(i, new Token(Lexem.GO_TRUE, String.valueOf(true_pos)));
+            }
+        }
+
+        while_result.add(new Token(Lexem.WHILE_KW, "END"));
+
+        return while_result;
+    }
+
+    public List<Token> doTranslationForBody() {
+        List<Token> body_result = new ArrayList<>();
+        while (current_token.getLexem() != Lexem.RIGHT_BR) {
+            body_result.addAll(doTranslationForExpressionOp(Lexem.SEMICOLON));
+        }
+        iterate();
+
+        return body_result;
+    }
+
+    public List<Token> doTranslationForExpressionOp(Lexem lexem) {
+        List<Token> expression_result = new ArrayList<>();
+        Stack<Token> stack = new Stack<>();
+        while (current_token.getLexem() != lexem) {
+            if (current_token.getLexem() == Lexem.DIGIT || current_token.getLexem() == Lexem.VAR ||
+                    current_token.getLexem() == Lexem.BOOL) {
+                expression_result.add(current_token);
+            }
+            else if (current_token.getLexem() == Lexem.ADDITION_OP || current_token.getLexem() == Lexem.SUBTRACTION_OP ||
+                    current_token.getLexem() == Lexem.MULTIPLICATION_OP || current_token.getLexem() == Lexem.DIVISION_OP ||
+                    current_token.getLexem() == Lexem.ASSIGN_OP || current_token.getLexem() == Lexem.MORE_LOGICAL_OP ||
+                    current_token.getLexem() == Lexem.LESS_LOGICAL_OP || current_token.getLexem() == Lexem.MORE_OR_EQUAL_LOGICAL_OP ||
+                    current_token.getLexem() == Lexem.LESS_OR_EQUAL_LOGICAL_OP || current_token.getLexem() == Lexem.EQUAL_LOGICAL_OP) {
+                if (stack.isEmpty()) {
+                    stack.push(current_token);
+                }
+                else {
+                    Integer stack_pr = lexemPriority.get(stack.peek().getLexem());
+                    Integer current_pr = lexemPriority.get(current_token.getLexem());
+                    while (stack_pr >= current_pr) {
+                        expression_result.add(stack.pop());
+                        if (stack.isEmpty()) {
+                            break;
+                        }
+                        else {
+                            stack_pr = lexemPriority.get(stack.peek().getLexem());
+                        }
+                    }
+                    stack.push(current_token);
+                }
+            }
+            else if (current_token.getLexem() == Lexem.LEFT_B) {
+                stack.push(current_token);
+            }
+            else if (current_token.getLexem() == Lexem.RIGHT_B) {
+                while (stack.peek().getLexem() != Lexem.LEFT_B) {
+                    expression_result.add(stack.pop());
+                }
+                stack.pop();
+            }
+            iterate();
+        }
+        iterate();
+        while (!stack.isEmpty()) {
+            expression_result.add(stack.pop());
+        }
+
+        return expression_result;
+    }
+
+    public List<Token> doTranslationForLogicalOp() {
+        List<Token> logical_result = new ArrayList<>();
+        iterate();
+        iterate();
+        logical_result.addAll(doTranslationForExpressionOp(Lexem.RIGHT_B));
+        logical_result.add(new Token(Lexem.GO_FALSE, "-1"));
+
+        return logical_result;
+    }
+}
---
+++ src/main/java/stack_machine/StackMachine.java
@@ -0,0 +1,133 @@
+package stack_machine;
+
+import lexer.Lexem;
+import token.Token;
+
+import java.util.*;
+
+public class StackMachine{
+    private final List<Token> tokens;
+    private Integer tokenIndex;
+    private Token current_token;
+    private final Map<String, Double> varTable = new HashMap<>();
+    private final Stack<Token> stack = new Stack<>();
+
+    public StackMachine(List<Token> tokens) {
+        this.tokens = tokens;
+        this.tokenIndex = -1;
+        this.current_token = null;
+    }
+
+    private void iterate() {
+        tokenIndex += 1;
+        if (tokenIndex < tokens.size()) {
+            current_token = tokens.get(tokenIndex);
+        }
+    }
+
+    public Map<String, Double> canculate() {
+        iterate();
+        doCanculate();
+
+        return varTable;
+    }
+
+    public void doCanculate() {
+        while (tokenIndex != tokens.size()) {
+            if (current_token.getLexem() == Lexem.DIGIT || current_token.getLexem() == Lexem.VAR ||
+                    current_token.getLexem() == Lexem.BOOL) {
+                stack.push(current_token);
+            } else if (current_token.getLexem() == Lexem.ADDITION_OP || current_token.getLexem() == Lexem.SUBTRACTION_OP ||
+                    current_token.getLexem() == Lexem.MULTIPLICATION_OP || current_token.getLexem() == Lexem.DIVISION_OP ||
+                    current_token.getLexem() == Lexem.ASSIGN_OP || current_token.getLexem() == Lexem.MORE_LOGICAL_OP ||
+                    current_token.getLexem() == Lexem.LESS_LOGICAL_OP || current_token.getLexem() == Lexem.MORE_OR_EQUAL_LOGICAL_OP ||
+                    current_token.getLexem() == Lexem.LESS_OR_EQUAL_LOGICAL_OP || current_token.getLexem() == Lexem.EQUAL_LOGICAL_OP) {
+                Token op = current_token;
+                Token f_element = stack.pop();
+                String first_element = f_element.getValue();
+                Token s_element = stack.pop();
+                String second_element = s_element.getValue();
+                if (op.getLexem() == Lexem.ASSIGN_OP) {
+                    doAssignOp(first_element, second_element);
+                } else {
+                    if (f_element.getLexem() == Lexem.VAR) {
+                        first_element = varTable.get(f_element.getValue()).toString();
+                    }
+                    if (s_element.getLexem() == Lexem.VAR) {
+                        second_element = varTable.get(s_element.getValue()).toString();
+                    }
+                    if (op.getLexem() == Lexem.ADDITION_OP || op.getLexem() == Lexem.SUBTRACTION_OP ||
+                            op.getLexem() == Lexem.MULTIPLICATION_OP || op.getLexem() == Lexem.DIVISION_OP) {
+                        stack.push(doArithmOp(Double.parseDouble(first_element), Double.parseDouble(second_element)));
+                    }
+                    if (op.getLexem() == Lexem.MORE_LOGICAL_OP || op.getLexem() == Lexem.LESS_LOGICAL_OP ||
+                            op.getLexem() == Lexem.MORE_OR_EQUAL_LOGICAL_OP || op.getLexem() == Lexem.LESS_OR_EQUAL_LOGICAL_OP ||
+                            op.getLexem() == Lexem.EQUAL_LOGICAL_OP) {
+                        stack.push(doLogicalOp(Double.parseDouble(first_element), Double.parseDouble(second_element)));
+                    }
+                }
+            }
+            else if (current_token.getLexem() == Lexem.GO_FALSE) {
+                Token tmp = stack.pop();
+                String value;
+                if (tmp.getLexem() == Lexem.VAR) {
+                    value = varTable.get(tmp.getValue()).toString();
+                }
+                else {
+                    value = tmp.getValue();
+                }
+                if (!Boolean.parseBoolean(value)) {
+                    tokenIndex = Integer.parseInt(current_token.getValue()) - 1;
+                }
+            }
+            else if (current_token.getLexem() == Lexem.GO_TRUE) {
+                tokenIndex = Integer.parseInt(current_token.getValue()) - 1;
+            }
+            iterate();
+        }
+    }
+
+    public void doAssignOp(String first_element, String second_element) {
+        varTable.put(first_element, Double.parseDouble(second_element));
+    }
+
+    public Token doLogicalOp(Double first_element, Double second_element) {
+        boolean logical_res = false;
+        if (current_token.getLexem() == Lexem.MORE_LOGICAL_OP) {
+            logical_res = second_element > first_element;
+        }
+        else if (current_token.getLexem() == Lexem.LESS_LOGICAL_OP) {
+            logical_res = second_element < first_element;
+        }
+        else if (current_token.getLexem() == Lexem.MORE_OR_EQUAL_LOGICAL_OP) {
+            logical_res = second_element >= first_element;
+        }
+        else if (current_token.getLexem() == Lexem.LESS_OR_EQUAL_LOGICAL_OP) {
+            logical_res = second_element <= first_element;
+        }
+        else if (current_token.getLexem() == Lexem.EQUAL_LOGICAL_OP) {
+            logical_res = second_element == first_element;
+        }
+
+        return new Token(Lexem.BOOL, Boolean.toString(logical_res));
+    }
+
+    public Token doArithmOp(Double first_element, Double second_element) {
+        Double arithm_res = 0.0;
+        if (current_token.getLexem() == Lexem.ADDITION_OP) {
+            arithm_res = second_element + first_element;
+        }
+        else if (current_token.getLexem() == Lexem.SUBTRACTION_OP) {
+            arithm_res = second_element - first_element;
+        }
+        else if (current_token.getLexem() == Lexem.MULTIPLICATION_OP) {
+            arithm_res = second_element * first_element;
+        }
+        else if (current_token.getLexem() == Lexem.DIVISION_OP) {
+            arithm_res = second_element / first_element;
+        }
+
+        return new Token(Lexem.DIGIT, arithm_res.toString());
+    }
+
+}
