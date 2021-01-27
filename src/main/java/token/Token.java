e09395b15e33eb91a2adf78c0b52db96f8e24e39
JaneKKTTme <tyryry221@gmail.com>
2021-01-28 02:57:14 +0300

    Add needed methods

--- src/main/java/token/Token.java
+++ src/main/java/token/Token.java
@@ -11,16 +11,63 @@ public class Token {
         this.value = value;
     }
 
-    public Lexem getLexem() {
+    public Lexem getType() {
         return lexem;
+    }
+
+    public void setType(Lexem lexem) {
+        this.lexem = lexem;
     }
 
     public String getValue() {
         return value;
     }
 
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     @Override
+    public int hashCode() {
+        final int a = lexem.hashCode();
+        final int b = value.hashCode();
+        final int x = 31;
+
+        return a * x + b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Token other = (Token) obj;
+
+        return lexem == other.lexem && value.equals(other.value);
+    }
+
     public String toString() {
-        return "[" + this.getLexem() + ": \"" + this.getValue() + '\"' + ']';
+        return this.getType() + " : \"" + this.getValue() + "\"";
+    }
+
+    public static Token fromString(String str) {
+        String[] typeAndValue = str.split("\\s+:\\s+");
+        Lexem type = Lexem.valueOf(typeAndValue[0]);
+
+        String value = typeAndValue[1];
+        if (value.length() > 2) {
+            value = value.substring(1, value.length() - 1);
+        }
+        else {
+            value = "";
+        }
+
+        return new Token(type, value);
     }
 }
